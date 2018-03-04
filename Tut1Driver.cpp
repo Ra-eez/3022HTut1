7b04dfd34fcd1e25c083159bb3a315171aa5e0a2
Ra-eez <eezy1997@gmail.com>
2018-03-05 00:33:05 +0200

    all functions except average function complete

--- Tut1Driver.cpp
+++ Tut1Driver.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "dbEngine.h"
-#include "cstdlib"
+#include "cstdlib"//used for clearing terminal function
 #include <string>
 #include <vector>
 #include <fstream>
@@ -11,19 +11,22 @@ using namespace std;
 
 int main(void){
 
+   //variables
    string selection;
    vector<STNMOE001::StudentRecord>studentVector;
    
-   //initially read in entries from the database/ txt file
+   //open file
    ifstream ifs("database");
    string line;
+   //parse through each line and enter values into the vector
    while(getline(ifs, line)){
       istringstream iss(line);
       string a, b, c, d; // a-name b-surname c-studentnumber d-classrecord
-      iss >> a >> b >> c >> d;
+      iss >> a >> b >> c;
+      getline(iss,d);
       STNMOE001::add_student(a,b,c,d,studentVector); //add student to the vector
    }
-   
+   ifs.close();//close file
   
    for(;;){ //infinite loop
    
@@ -37,39 +40,82 @@ int main(void){
           <<"Enter a number (or q to quit) and press return...\n";
           
       cin >> selection;
-      system("clear");
+      system("clear");//clears terminal
       if (selection == "q") break; //terminating condition
       
-      else if (selection == "0"){ //add student
+      //add student
+      else if (selection == "0"){
       
-         string name; string surname; string studentnumber; string classrecord;
-         cout << "Enter student's name:\n";
+         cout << "Add student function selected\n\n";
+         string name; string surname; string studentnumber; string classrecord; string ya;
+         cout << "Enter student's first name:\n";
          cin >> name;
          cout << "Enter student's surname:\n";
          cin >> surname;
          cout << "Enter student's studentnumber:\n";
          cin >> studentnumber;
          cout << "Enter student's classrecord:\n";
-         cin >> classrecord;
+         cin.ignore(256, '\n');
+         getline(cin,classrecord);
          
          STNMOE001::add_student(name,surname,studentnumber,classrecord,studentVector);
 
       }
-      else if (selection == "1"){ //read database
-         cout << "1 selected\n";
+      //read database
+      else if (selection == "1"){
+         cout << "Read database function selected\n\n";
+         
          for (int i=0; i<studentVector.size();i++){
             STNMOE001::StudentRecord Student = studentVector[i];
-            cout << Student.Name;
+            cout << "Student name:    " << Student.Name
+                 << "\nStudent surname: " << Student.Surname
+                 << "\nStudent number:  " << Student.StudentNumber
+                 << "\nClass record:   " << Student.ClassRecord << "\n\n";
          }
       }
-      else if (selection == "2"){ //save database
-         cout << "2 selected\n";
-      }
+      //save database
+      else if (selection == "2"){
+         cout << "Save database function selected\n\n";
+         
+         ofstream file;
+         file.open("database");
+         for (int i=0; i<studentVector.size();i++){
+            STNMOE001::StudentRecord Student = studentVector[i];
+            string details;
+            details = Student.Name + " " + Student.Surname + " " + Student.StudentNumber + " " + Student.ClassRecord + "\n";
+            file << details;
+         }
+         file.close();
+               }
       else if (selection == "3"){ //display given student
-         cout << "3 selected\n";
+         cout << "3 selected\nPlease enter the student number:";
+         string sn;
+         int counter = 0; //when student is found this becomes 1, if student not found then this will indicate so
+         cin >> sn;
+         for (int i=0; i<studentVector.size();i++){
+            STNMOE001::StudentRecord Student = studentVector[i];
+            if (Student.StudentNumber == sn){
+               cout << Student.Name;
+               counter = 1;
+            }
+         }
+         if (counter == 0) (cout << "Student not found");
+            
       }
       else if (selection == "4\n"){ //grade student
-         cout << "4 selected";
+         cout << "4 selected\nPlease enter the student number:";
+         string sn;
+         int counter = 0; //when student is found this becomes 1, if student not found then this will indicate so
+         cin >> sn;
+         for (int i=0; i<studentVector.size();i++){
+            STNMOE001::StudentRecord Student = studentVector[i];
+            if (Student.StudentNumber == sn){
+            //average calcultions and printing here
+               cout << Student.Name;
+               counter = 1;
+            }
+         }
+         if (counter == 0) (cout << "Student not found");
       }
    }
    
--- dbEngine.cpp
+++ dbEngine.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 //add student method
 void STNMOE001::add_student(string name, string surname, string studentnumber, string classrecord, vector<StudentRecord>&studentVector){
-
-   StudentRecord newPerson = {name,surname,studentnumber,classrecord};
-   studentVector.push_back(newPerson);
+   
+   //ensure there are no students with the same student number
+   int counter = 0; //beocomes 1 if there is someone of the same name already inputted
+   for (int i=0; i<studentVector.size();i++){
+      STNMOE001::StudentRecord Student = studentVector[i];
+      string current = Student.StudentNumber;
+      if (current == studentnumber){
+         counter = 1;
+      }
+   }
+   if (counter == 0){
+      StudentRecord newPerson = {name,surname,studentnumber,classrecord};
+      studentVector.push_back(newPerson);
+   }
    }
